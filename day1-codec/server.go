0e1e9af7545bc710580601cea1a508788ee61311
Qiao Liu <qiao.liu@iglooinsure.com>
2021-04-26 18:54:21 +0800

    define codec and server base function

---
+++ day1-codec/server.go
@@ -0,0 +1,47 @@
+package goorpc
+
+import (
+	"goorpc/codec"
+	"io"
+	"log"
+	"net"
+)
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int        // MagicNumber marks this's a geerpc request
+	CodecType   codec.Type // client may choose different Codec to encode body
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+var DefaultServer = NewServer()
+
+func (s *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go s.ServeConn(conn)
+	}
+}
+
+func (s *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() {
+		_ = conn.Close()
+	}()
+}
+
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
